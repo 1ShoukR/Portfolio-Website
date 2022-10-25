8422ab944e211de58fb32966b85e29d86d739133
1ShoukR <rahminshoukoohi@gmail.com>
2022-10-25 16:35:38 -0400

    added my resume page

--- client/src/components/Description.jsx
+++ client/src/components/Description.jsx
@@ -4,40 +4,48 @@ import { Card } from '@mui/material';
 
 const Description = () => {
 return (
-    <>
-    <div className="myPic max-w-sm w-full lg:max-w-full lg:flex">
-        <Card
-            sx={{
-            display: 'flex',
-            width: '920px',
-            flexDirection: 'row',
-            padding: '25px',
-            borderRadius: '16px',
-            boxShadow: 3,
-            bgcolor: 'text.secondary',
-        }}
-        >
-        <div className="pictureContainer">
-            <img
-            className="myPicSpecifics rounded-md"
-            src={MyPic}
-            alt="picture of me"
-            />
-        </div>
-        <div className="description">
-            <p>
-            I am incredibly passionate about all things tech! I have attended
-            and graduated the DigitalCrafts Web Development Bootcamp to learn
-            Full-Stack Web Development in order to jumpstart my career in
-            tech, and it is the best decision I have ever made in my life. My
-            love for coding continues to grow exponentially as I learn more
-            and more about the intricacies and nuances of each language that I
-            pursue!
-            </p>
-        </div>
-        </Card>
-    </div>
-    </>
+	<>
+		<div className="myPic max-w-sm w-full lg:max-w-full lg:flex">
+			<Card
+				sx={{
+					display: 'flex',
+					width: '920px',
+					flexDirection: 'row',
+					padding: '25px',
+					borderRadius: '16px',
+					boxShadow: 3,
+					bgcolor: 'text.secondary',
+				}}>
+				<div className="pictureContainer">
+					<img
+						className="myPicSpecifics rounded-md"
+						src={MyPic}
+						alt="me"
+					/>
+				</div>
+				<div className="description">
+					<p>
+						I am incredibly passionate about all things tech! I have attended
+						and graduated the DigitalCrafts Web Development Bootcamp to learn
+						Full-Stack Web Development in order to jumpstart my career in tech,
+						and it is the best decision I have ever made in my life. My love for
+						coding continues to grow exponentially as I learn more and more
+						about the intricacies and nuances of each language that I pursue!
+					</p>
+					<div>
+						<a href="/Resume">
+							<button>Resume</button>
+						</a>
+					</div>
+					<div>
+						<a href="/Contact_Me">
+							<button>Contact Me</button>
+						</a>
+					</div>
+				</div>
+			</Card>
+		</div>
+	</>
 );
 };
 
--- client/src/components/Resume.jsx
+++ client/src/components/Resume.jsx
@@ -1,9 +1,276 @@
 import React from 'react'
+import me from "../assets/me.jpg"
+import "../Css/resume.css"
 
 const Resume = () => {
   return (
-    <div>Resume</div>
-  )
+		<div className="wrapper">
+			<div className="container">
+				<div className="left_Side">
+					<div className="profileText">
+						<div className="imgBx">
+							<img alt='me' className="photo" src={me} />
+						</div>
+						<br />
+						<h2>
+							Rahmin Shoukoohi <br />
+							<span>Web Developer</span>
+						</h2>
+					</div>
+					<div className="contactInfo">
+						<h3 className="title">Contact Info</h3>
+						<ul>
+							<li>
+								<span className="icon">
+									<i className="fa fa-phone" aria-hidden="true"></i>
+								</span>
+								<span className="text">770-925-5248</span>
+							</li>
+							<li>
+								<span className="icon">
+									<i className="fa fa-envelope-o" aria-hidden="true"></i>
+								</span>
+								<span className="text">
+									<a href="mailto: rahminshoukoohi@gmail.com">Send Email</a>
+								</span>
+							</li>
+							<li>
+								<span className="icon">
+									<i className="fa fa-globe" aria-hidden="true"></i>
+								</span>
+								<span className="text">
+									<a href="https://rahminshoukoohi.wixsite.com/mysite">
+										Portfolio
+									</a>
+								</span>
+							</li>
+							<li>
+								<span className="icon">
+									<i className="fa fa-linkedin" aria-hidden="true"></i>
+								</span>
+								<span className="text">
+									<a href="https://www.linkedin.com/in/rahmin-shoukoohi-155855235/">
+										LinkedIn
+									</a>
+								</span>
+							</li>
+							<li>
+								<span className="icon">
+									<i className="fa fa-map-marker" aria-hidden="true"></i>
+								</span>
+								<span className="text">Atlanta, Georgia</span>
+							</li>
+						</ul>
+					</div>
+					<div className="contactInfo education">
+						<h3 className="title">Education</h3>
+						<ul>
+							<li>
+								<h5>2018 - 2022</h5>
+								<h4>Bachelor's Degree in Biology Pre-Dental</h4>
+								<h4>Georgia State University</h4>
+							</li>
+							<li>
+								<h5>2022 - 2022</h5>
+								<h4>
+									Certificate of Completion: Computer Science and Software
+									Development
+								</h4>
+								<h4>DigitalCrafts Web Development Bootcamp</h4>
+							</li>
+							<li>
+								<h5>1997 - 2007</h5>
+								<h4>Matriculation </h4>
+								<h4>University Name</h4>
+							</li>
+						</ul>
+					</div>
+					<div className="contactInfo language">
+						<h3 className="title">Languages</h3>
+						<ul>
+							<li>
+								<span className="text">English</span>
+								<span className="percent">
+									<div data-language="english"></div>
+								</span>
+							</li>
+						</ul>
+						<ul>
+							<li>
+								<span className="text">Russian</span>
+								<span className="percent">
+									<div data-language="russian"></div>
+								</span>
+							</li>
+						</ul>
+						<ul>
+							<li>
+								<span className="text">French</span>
+								<span className="percent">
+									<div data-language="french"></div>
+								</span>
+							</li>
+						</ul>
+					</div>
+				</div>
+				<div className="right_Side">
+					<div className="about">
+						<h2 className="title2">Profile</h2>
+						<p>
+							As a passionate and motivated professional, I’m constantly
+							striving to improve my techniques, expand my skillset and find new
+							opportunities to grow. Each of my projects - both solo and
+							collaborative - have provided this growth and allowed me to
+							establish myself within this competitive industry. Please enjoy my
+							portfolio, and feel free to get in touch with any questions.
+						</p>
+					</div>
+					<div className="about">
+						<h2 className="title2">Experience</h2>
+						<div className="box">
+							<div className="year_company">
+								<h5>2021 - 2022</h5>
+								<h5>Built by Gamers</h5>
+							</div>
+							<div className="text">
+								<h4>Professional Esports Athlete</h4>
+								<p>
+									● Engaged in team oriented practices that include, but is no
+									limited to, a team of 7 individuals <br /> ● Oriented the
+									practice flow from a week-to-week basis, and provided quality
+									performance reviews to higher ups <br /> ● Traveled to
+									multiple competitions both domestically and internationally{' '}
+									<br /> ● Engaged in fan-revolved activities, including signing
+									sessions, interviews, meet-and-greets, public appearances, and
+									live competition for viewing purposes
+								</p>
+							</div>
+						</div>
+
+						<div className="box">
+							<div className="year_company">
+								<h5>2022 - 2022</h5>
+								<h5>DigitalCrafts Web Development Bootcamp</h5>
+							</div>
+							<div className="text">
+								<h4>Software Developer</h4>
+								<p>
+                  ● Enganged in classroom study of web development tools, including, but not limited to, Node.JS,
+                  Express.JS, REST APIs, Python, Git, SQL, PostgreSQL, Sequelize, Beekeeper, Postman, React,
+                  Redux, Javascript, Typescript, HTML5, APIs, CSS, and CSS Frameworks<br />
+                  ● Participated in SCRUM and AGILE methodologies while developing software
+                </p>
+							</div>
+						</div>
+						<div className="box">
+							<div className="year_company">
+								<h5>2016 - 2018</h5>
+								<h5>Company Name</h5>
+							</div>
+							<div className="text">
+								<h4>Junior UX Developer</h4>
+								<p>
+									Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+									do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+								</p>
+							</div>
+						</div>
+					</div>
+					<div className="skillsContainer">
+						<div className="leftSideSkills">
+							<div className="about skills">
+								<h2 className="title2">Professionals skills</h2>
+								<div className="box">
+									<h1 className="techStack">Frontend</h1>
+								</div>
+								<div className="box">
+									<h4>React.JS</h4>
+								</div>
+								<div className="box">
+									<h4>Redux</h4>
+								</div>
+								<div className="box">
+									<h4>JavaScript ES6</h4>
+								</div>
+								<div className="box">
+									<h4>Typescript</h4>
+								</div>
+								<div className="box">
+									<h4>HTML5</h4>
+								</div>
+								<div className="box">
+									<h4>APIs</h4>
+								</div>
+								<div className="box">
+									<h4>CSS</h4>
+								</div>
+								<div className="box">
+									<h4>CSS Frameworks</h4>
+								</div>
+							</div>
+						</div>
+						<div className="about skills">
+							<div className="rightSideSkills">
+								<div className="box">
+									<h1 className="techStack">Backend</h1>
+								</div>
+								<div className="box">
+									<h4>Node.JS</h4>
+								</div>
+								<div className="box">
+									<h4>Express.JS</h4>
+								</div>
+								<div className="box">
+									<h4>REST APIs</h4>
+								</div>
+								<div className="box">
+									<h4>Python</h4>
+								</div>
+								<div className="box">
+									<h4>Git</h4>
+								</div>
+								<div className="box">
+									<h4>GitHub</h4>
+								</div>
+								<div className="box">
+									<h4>SQL</h4>
+								</div>
+								<div className="box">
+									<h4>PostgreSQL</h4>
+								</div>
+								<div className="box">
+									<h4>Sequelize</h4>
+								</div>
+								<div className="box">
+									<h4>Beekeeper Database Interface</h4>
+								</div>
+								<div className="box">
+									<h4>Postman Frontend Simulator</h4>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div className="about interest">
+						<h2 className="title2">Interests</h2>
+						<ul>
+							<li>
+								<i className="fa fa-book" aria-hidden="true"></i> Reading
+							</li>
+							<li>
+								<i className="fa fa-gamepad" aria-hidden="true"></i> Gaming
+							</li>
+							<li>
+								<i className="fa fa-cutlery" aria-hidden="true"></i> Cooking
+							</li>
+							<li>
+								<i className="fa fa-microphone" aria-hidden="true"></i> Singing
+							</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default Resume
