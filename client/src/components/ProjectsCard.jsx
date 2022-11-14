873e946521a285825484a337bca1b7ec22951909
1ShoukR <rahminshoukoohi@gmail.com>
2022-11-14 16:17:13 -0500

    added mobile responsiveness to projects page

--- client/src/components/ProjectsCard.jsx
+++ client/src/components/ProjectsCard.jsx
@@ -5,25 +5,44 @@ import "../Css/projectsCard.css"
 
 const ProjectsCard = (props) => {
   return (
-    <div className='projectsCardContainer'>
-      {props?.projects[0]?.map((project) => (
-        <Card
-        sx={{
-            display: 'flex',
-            marginTop: '20px',
-            width: '920px',
-            padding: '20px',
-            flexDirection: 'column-reverse',
-            borderRadius: '16px',
-            boxShadow: 3,
-            bgcolor: 'text.secondary',
-          }}
-        >
-          <ProjectCardInfo project={project} />
-        </Card>
-      ))}
-    </div>
-  );
+		<div className="projectsCardContainer">
+			{props?.projects[0]?.map((project) => (
+				<Card
+					sx={{
+						display: 'flex',
+						width: {
+							xs: '480px',
+							sm: '760px',
+							md: '1000px',
+						},
+						flexDirection: {
+							xs: 'column',
+							md: 'row',
+							lg: 'row',
+							xl: 'row',
+						},
+						padding: '40px',
+						borderRadius: '16px',
+						boxShadow: 3,
+						bgcolor: 'text.secondary',
+						gap: '30px',
+						justifyContent: {
+							xs: 'center',
+							md: 'center',
+							lg: 'center',
+							xl: 'center',
+						},
+						alignItems: {
+							xs: 'center',
+							md: 'center',
+							lg: 'center',
+						},
+					}}>
+					<ProjectCardInfo project={project} />
+				</Card>
+			))}
+		</div>
+	);
 }
 
 export default ProjectsCard
